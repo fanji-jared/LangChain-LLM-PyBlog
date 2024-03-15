c060ca7191cddd65bd3becda78a6a9d9cc0f91df
fanji-jared <2967557176@qq.com>
2024-03-16 00:00:07 +0800

    修改了静态资源地址为共用地址，成功运行

--- my_site/settings.py
+++ my_site/settings.py
@@ -144,11 +144,12 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# 采取统一static
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static/'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
